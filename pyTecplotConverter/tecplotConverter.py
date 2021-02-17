cf9ef827b18baf290a58392cd9197af5ecd1286a
Debartha Chakraborty <deba32cty@gmail.com>
2021-02-18 01:30:42 +0530

    Added multiple file input for tecplotConverter.py

--- pyTecplotConverter/tecplotConverter.py
+++ pyTecplotConverter/tecplotConverter.py
@@ -8,7 +8,7 @@ from tecio_szl import open_file, zone_write_double_values,\
 nn = 1601441
 ne = 4659888
 ndof = 5
-nvar = 5
+nvar = 7
 gamma = 1.4
 cv = 1/(gamma*(gamma-1))
 x = np.empty(nn, dtype='float64')
@@ -19,8 +19,10 @@ d = np.empty((nn,ndof), dtype='float64')
 d_trans = np.empty((nn,nvar), dtype='float64')
 
 #%% Filenames
-dataFile = 'data.0012'
-outFile = 'Re10k_bp1p6_8000float.szplt'
+#dataFile = ['data.'+ str(i+1).zfill(2) for i in range(28)]
+#outFile = ['Re10k_bp1p4_' + str(i+1) + '000.szplt' for i in range(28)]
+dataFile = ['data.28']
+outFile = ['Re10k_bp1p4_28000.szplt']          
 xyzFile = 'xyz.bin'
 ienFile = 'ien.bin'
 
@@ -39,34 +41,35 @@ for i in range(ne):
 xyzfile.close()
 ienfile.close()
 
-#%% Read data
-datafile = open(dataFile,'rb')
-for i in range(nn):
-    for j in range(ndof):
-        data = datafile.read(8)
-        d[i,j] = struct.unpack('d', data)[0]
-    d_trans[i,0] = d[i,0]
-    d_trans[i,1] = d[i,1]/d[i,0]
-    d_trans[i,2] = d[i,2]/d[i,0]
-    d_trans[i,3] = d[i,3]/d[i,0]
-    d_trans[i,4] = (1/cv)*(d[i,4]/d[i,0] - 0.5*(d_trans[i,1]**2 \
-                         + d_trans[i,2]**2 + d_trans[i,3]**2))
-#    d_trans[i,5] = (d_trans[i,1]**2 + d_trans[i,2]**2 \
-#                    + d_trans[i,3]**2)**0.5/d_trans[i,4]**0.5
-#    d_trans[i,6] = d_trans[i,0]*d_trans[i,4]/gamma
-datafile.close()
-
-#%% Write data
-varNames = ['x','y','z','rho','uvel','vvel','wvel','temp']
-file_handle = open_file(outFile, "Y-duct", varNames)
-zone = create_FE_zone(file_handle, "Zone", nn, ne, FEBRICK, None, \
+#%% Read and write data
+for iFile, dataFilename in enumerate(dataFile):
+    datafile = open(dataFilename,'rb')
+    for i in range(nn):
+        for j in range(ndof):
+            data = datafile.read(8)
+            d[i,j] = struct.unpack('d', data)[0]
+        d_trans[i,0] = d[i,0]
+        d_trans[i,1] = d[i,1]/d[i,0]
+        d_trans[i,2] = d[i,2]/d[i,0]
+        d_trans[i,3] = d[i,3]/d[i,0]
+        d_trans[i,4] = (1/cv)*(d[i,4]/d[i,0] - 0.5*(d_trans[i,1]**2 \
+                     + d_trans[i,2]**2 + d_trans[i,3]**2))
+        d_trans[i,5] = (d_trans[i,1]**2 + d_trans[i,2]**2 \
+                     + d_trans[i,3]**2)**0.5/d_trans[i,4]**0.5
+        d_trans[i,6] = d_trans[i,0]*d_trans[i,4]/gamma
+    datafile.close()
+    varNames = ['x','y','z','rho','uvel','vvel','wvel','temp','mach','pressure']
+    file_handle = open_file(outFile[iFile], "Y-duct", varNames)
+    print(outFile[iFile] + ' opened.')
+    zone = create_FE_zone(file_handle, "Zone", nn, ne, FEBRICK, None, \
                                [FD_FLOAT for i in range(len(varNames))])
-zone_write_float_values(file_handle, zone, 1, x)
-zone_write_float_values(file_handle, zone, 2, y)
-zone_write_float_values(file_handle, zone, 3, z)
-for i in range(4, len(varNames) + 1):
-    zone_write_float_values(file_handle, zone, i, d_trans[:, i - 4])
-zone_write_nodemap(file_handle, zone, ien.reshape(-1))
-close_file(file_handle)
+    zone_write_float_values(file_handle, zone, 1, x)
+    zone_write_float_values(file_handle, zone, 2, y)
+    zone_write_float_values(file_handle, zone, 3, z)
+    for i in range(4, len(varNames) + 1):
+        zone_write_float_values(file_handle, zone, i, d_trans[:, i - 4])
+    zone_write_nodemap(file_handle, zone, ien.reshape(-1))
+    close_file(file_handle)
+    print(outFile[iFile] + ' closed.')
 
 
